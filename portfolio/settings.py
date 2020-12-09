c63e3b2636dd85cf9705760bef0e72356d82a5f9
avosa <websterb17@gmail.com>
2020-12-09 23:18:02 +0000

    modified settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'Enter Your Secret Key'
+SECRET_KEY = 'qqqqqqqqddddd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

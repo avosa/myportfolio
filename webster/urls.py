6d39329169ad1db61d34a7afe82abc286a42bddc
avosa <websterb17@gmail.com>
2020-12-10 11:30:56 +0000

    Fixed webster.urls

--- webster/urls.py
+++ webster/urls.py
@@ -5,7 +5,7 @@ from . import views #import views
 # adding url
 urlpatterns = [
     path('', views.index, name='home'), #home
-    path('project', views.project, name='project'), #project
+    path('projects', views.project, name='project'), #project
     path('contact', views.contact, name='contact'), #contact
     path('about', views.about, name='about'), #about
 ]
